03af3e32e669dfdf5fb3433accba49ff3ba3ce23
Sam Wheating <sam.wheating@shopify.com>
2020-03-15 16:06:11 -0400

    fixed issues in deployment

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ IMAGE=gcr.io/personalsite-264919/battlesnake:latest
 PROJECT=personalsite-264919
 
 gcloud config set account samwheating@gmail.com
-docker build -t $IMAGE .
+docker build --no-cache -t $IMAGE .
 docker push $IMAGE
-gcloud run deploy $SERVICE_NAME --image $IMAGE --region us-central1 --platform managed --project $PROJECT
+
+# Deploy to cloud run in us-west-1 (The same zone as the battlesnake gameserver)
+gcloud run deploy $SERVICE_NAME --image $IMAGE --region us-west1 --platform managed --project $PROJECT
