7234d791bd39bbfa86454d7ada588398f804743a
Sam Wheating <sam.wheating@shopify.com>
2020-12-11 13:55:43 -0500

    Inverting y-axis, fixing tests

--- pkg/lookahead/lookahead_test.go
+++ pkg/lookahead/lookahead_test.go
@@ -7,57 +7,6 @@ import (
 	"github.com/SamWheating/battlesnake2020/pkg/structs"
 )
 
-func TestNextBoards(t *testing.T) {
-	state := structs.MoveRequest{
-		Game: structs.Game{
-			ID: "666",
-		},
-		Turn: 4,
-		You: structs.Snake{
-			Body: []structs.Coordinate{
-				{
-					X: 0,
-					Y: 1,
-				}, {
-					X: 1,
-					Y: 1,
-				},
-			},
-		},
-		Board: structs.Board{
-			Height: 2,
-			Width:  2,
-			Snakes: []structs.Snake{
-				{
-					Body: []structs.Coordinate{
-						{
-							X: 0,
-							Y: 1,
-						}, {
-							X: 1,
-							Y: 1,
-						},
-					},
-				}, {
-					Body: []structs.Coordinate{
-						{
-							X: 3,
-							Y: 4,
-						}, {
-							X: 3,
-							Y: 5,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	for board := range lookahead.NextBoards(state, 4) {
-		t.Logf("The turn is...%+v", board)
-	}
-}
-
 func TestIsStarved(t *testing.T) {
 	snake := structs.Snake{
 		Health: 100,
@@ -289,20 +238,6 @@ func TestApplyMovesToBoardMovesOneSnakeHitsWall(t *testing.T) {
 	}
 	if nextboard.Snakes[0].ID == "lost" {
 		t.Errorf("The wrong snake died")
-	}
-}
-
-func TestGetSnakeMoves(t *testing.T) {
-	board := structs.Board{
-		Snakes: []structs.Snake{
-			structs.Snake{
-				ID: "snake1",
-			},
-		},
-	}
-	moves := lookahead.GetSnakeMoves(board, 1)
-	if len(moves) != 4 {
-		t.Errorf("Actual output not as expected")
 	}
 }
 
